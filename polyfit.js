12ee39157c0c8d3a6b92d5554bae04651d9204c8
raibosome <raimi.bkarim@gmail.com>
2020-02-28 07:08:13 +0800

    [WIP]

--- polyfit.js
+++ polyfit.js
@@ -1,15 +1,15 @@
-$(function() {
-  var MAX_MODEL_DEGREE = 7;
-  var modelDegree = 0;
+$(function () {
+  var MAX_MODEL_DEGREE = 10;
+  var modelDegree = 1;
   $('#decrement-degree').attr('disabled', 'disabled');
 
   var MIN_NUM_POINTS = 5;
-  var MAX_NUM_POINTS = 30;
+  var MAX_NUM_POINTS = 100;
   var numPoints = parseInt($('#num-points').text(), 10);
 
   var $noiseSlider = $('#noise-slider');
 
-  var margin = {top: 0, right: 0, bottom: 20, left: 40}
+  var margin = { top: 10, right: 10, bottom: 20, left: 40 }
   var width = $('#train-scatterplot').width() - margin.left - margin.right;
   var height = width - margin.top - margin.bottom;
 
@@ -27,7 +27,7 @@ $(function() {
     .attr('height', height)
     .attr('class', 'main')
 
-    // the chart object, includes all margins
+  // the chart object, includes all margins
   var testChart = d3.select('#test-scatterplot')
     .append('svg:svg')
     .attr('width', width + margin.right + margin.left)
@@ -46,40 +46,36 @@ $(function() {
   // Data
   var data = {
     // These will be shown on the plot
-    trainPoints : {
-      xdata : [],
-      ydata : []
+    trainPoints: {
+      xdata: [],
+      ydata: []
     },
     // These will be for evaluation purposes only
-    testPoints : {
-      xdata : [],
-      ydata : [],
+    testPoints: {
+      xdata: [],
+      ydata: [],
     }
   }
 
   var errors = {
-    trainErrors : [],
-    trainErrorsRaimi : [],
-    testErrors : [],
+    trainErrors: [],
+    trainErrorsRaimi: [],
+    testErrors: [],
   }
 
   // Initial noise. This is what the slider is set to.
-  // 0 means data is exactly polynomial
-  var randomness = 30;
-
+  var randomness = 0;
   rangeSlider($noiseSlider[0], {
-    value: randomness,
+    value: 15,
     drag: function(value) {
-      // Update the randomness after the user drags the slider
-      // and reset the points to be clustered
-      randomness = value;
+      randomness = value/100;
       resetPoints();
     }
   });
 
   resetPoints();
 
-  function generateParams(numPoints) {
+  function generateParams() {
     var K = 5;
     var a = [];
     var w = [];
@@ -87,16 +83,16 @@ $(function() {
     var sign = Math.random() > 0.5 ? 1 : -1
     for (var i = 0; i < K; i++) {
       a.push(Math.random());
-      w.push(Math.random() * 10);
+      w.push(Math.random() * 5);
       theta.push(Math.random());
     }
     return {
-      K : K,
-      a : a,
-      w : w,
-      theta : theta,
-      sign : sign,
-      variance : Math.pow(randomness + numPoints, 1) / 500
+      K: K,
+      a: a,
+      w: w,
+      theta: theta,
+      sign: sign,
+      variance: randomness
     }
   }
 
@@ -110,13 +106,29 @@ $(function() {
     return result;
   }
 
-  function generateData(params, numPoints, xmin, xmax) {
+  function generateX(numPoints, xmin, xmax) {
     // The split between points following the polynomial and random points
     // is chosen arbitrarily, so the plot looks nice
     xmin = xmin || 0;
     xmax = xmax || 1;
 
     var xdata = [];
+
+    for (var i = 0; i < numPoints; i++) {
+      x = Math.random() * (xmax - xmin) + xmin;
+      xdata.push(x);
+    }
+
+    return xdata;
+  }
+
+  function generateData(params, numPoints, xdata) {
+    // The split between points following the polynomial and random points
+    // is chosen arbitrarily, so the plot looks nice
+    // xmin = xmin || 0;
+    // xmax = xmax || 1;
+
+    // var xdata = [];
     var ydata = [];
     var epsilons = [];
 
@@ -124,8 +136,9 @@ $(function() {
     // y = sum_{i=1}^K e^{a_i t} cos (w_i t + theta_i)
     // Raw f(x)
     for (var i = 0; i < numPoints; i++) {
-      x = Math.random() * (xmax - xmin) + xmin;
-      xdata.push(x);
+      // x = Math.random() * (xmax - xmin) + xmin;
+      // xdata.push(x);
+      x = xdata[i];
 
       y = 0;
       for (var j = 0; j < params.K; j++) {
@@ -137,35 +150,34 @@ $(function() {
 
     // f(x)
     ydata = normalizeList(ydata);
-    console.log("ydata", ydata);
-
-    // Bias
-    var sum = ydata.reduce((a, b) => a + b, 0);
-    var avg = (sum / ydata.length) || 0;
-    var biasSquared = ydata.map(y => (y-avg)**2).reduce((a,b) => a+b, 0)**2;
+    fx = Array.from(ydata);
+    
 
     // f(x) + e
     for (var i = 0; i < ydata.length; i++) {
-      var epsilon = random_normal(0, params.variance)
+      var epsilon = random_normal(0, params.variance);
       epsilons.push(epsilon);
       ydata[i] += epsilon;
       ydata[i] = Math.max(ydata[i], 0);
       ydata[i] = Math.min(ydata[i], 1);
     }
+    console.log(fx);
+    console.log(ydata);
 
-    var sum = epsilons.reduce((a, b) => a + b, 0);
-    var avg = (sum / ydata.length) || 0;
-    var sigma = epsilons.map(epsilon => (epsilon-avg)**2).reduce((a,b) => a+b,0);
+    var avg = epsilons.reduce((a, b) => a + b, 0) / epsilons.length || 0;
+    var sampleVar = epsilons.map(epsilon => (epsilon - avg) ** 2).reduce((a, b) => a + b, 0) / (epsilons.length - 1);
 
-    $('#train-bias').text(biasSquared.toPrecision(3));
-    $('#sigma1').text(sigma.toPrecision(3));
-    $('#sigma2').text(params.variance.toPrecision(3));
+    $('#sigma1').text(sampleVar.toPrecision(3));
+    $('#sigma2').text(randomness.toPrecision(3));
+
 
     return {
-      xdata : xdata,
-      ydata : ydata
+      xdata: xdata,
+      ydata: ydata
     }
   }
+
+
 
   function resetPoints() {
     // Arbitrarily chosen variance and percentageClusteredPoints
@@ -173,18 +185,19 @@ $(function() {
     // with the slider.
     var numPoints = parseInt($('#num-points').text(), 10);
     var params = generateParams(numPoints);
-    data.trainPoints = generateData(params, numPoints);
-    data.testPoints = generateData(params, numPoints);
+    var xdata = generateX(numPoints);
+    data.trainPoints = generateData(params, numPoints, xdata);
+    data.testPoints = generateData(params, numPoints, xdata);
     errors = {
-      trainErrors : [],
-      testErrors : [],
+      trainErrors: [],
+      testErrors: [],
     }
 
     resetPlot(trainMain, data.trainPoints.xdata, data.trainPoints.ydata);
     resetPlot(testMain, data.testPoints.xdata, data.testPoints.ydata);
 
     // Reset model degree
-    modelDegree = 0;
+    modelDegree = 1;
     $('#increment-degree').removeAttr('disabled');
     $('#decrement-degree').attr('disabled', 'disabled');
 
@@ -206,12 +219,12 @@ $(function() {
     var x = d3.scale.linear()
       .domain([0, 1])
       // .domain([d3.min(xdata), d3.max(xdata)])  // the range of the values to plot
-      .range([ 0, width ]);        // the pixel range of the x-axis
+      .range([0, width]);        // the pixel range of the x-axis
 
     var y = d3.scale.linear()
       .domain([0, 1])
       // .domain([d3.min(ydata), d3.max(ydata)])
-      .range([ height, 0 ]);
+      .range([height, 0]);
 
     var xAxis = d3.svg.axis().scale(x).orient('bottom');
     var yAxis = d3.svg.axis().scale(y).orient('left').ticks(9);
@@ -219,18 +232,17 @@ $(function() {
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
     main.append('g')
-        .attr('transform', 'translate(0,0)')
-        .attr("class", "y axis")
-        .call(yAxis);
+      .attr('transform', 'translate(0,0)')
+      .attr("class", "y axis")
+      .call(yAxis);
     // draw the graph object
     var g = main.append("svg:g");
 
-    // console.log(ydata);
     g.selectAll("scatter-dots")
       .data(ydata)  // using the values in the ydata array
       .enter().append("svg:circle")  // create a new circle for each value
-      .attr("cy", function (d) { return y(d); } ) // translate y value to a pixel
-      .attr("cx", function (d,i) { return x(xdata[i]); } ) // translate x value
+      .attr("cy", function (d) { return y(d); }) // translate y value to a pixel
+      .attr("cx", function (d, i) { return x(xdata[i]); }) // translate x value
       .attr("r", 2) // radius of circle
       .style("opacity", 1.0); // opacity of circle
   }
@@ -303,22 +315,22 @@ $(function() {
     // x and y scales
     var x = d3.scale.linear()
       .domain([d3.min(xdata), d3.max(xdata)])  // the range of the values to plot
-      .range([ 0, width ]);        // the pixel range of the x-axis
+      .range([0, width]);        // the pixel range of the x-axis
 
     var y = d3.scale.linear()
       .domain([d3.min(points.ydata), d3.max(points.ydata)])
-      .range([ height, 0 ]);
+      .range([height, 0]);
 
     var lineFunction = d3.svg.line()
-        .x(function(d, i) { return x(xdata[i]); })
-        .y(function(d, i) { return y(ydata[i]); })
-        .interpolate("linear");
+      .x(function (d, i) { return x(xdata[i]); })
+      .y(function (d, i) { return y(ydata[i]); })
+      .interpolate("linear");
     var lineGraph = main.append('path')
-        .attr('class', 'regression-path')
-        .attr('d', lineFunction(ydata))
-        .attr("stroke", "blue")
-        .attr("stroke-width", 2)
-        .attr("fill", "none");
+      .attr('class', 'regression-path')
+      .attr('d', lineFunction(ydata))
+      .attr("stroke", "blue")
+      .attr("stroke-width", 2)
+      .attr("fill", "none");
   }
 
   function rmsError(coefficients, xdata, ydata) {
@@ -328,14 +340,23 @@ $(function() {
   }
 
   function msError(coefficients, xdata, ydata) {
+    console.log(ydata)
     var yhat = yHat(xdata, coefficients);
     var errors = numeric.sub(ydata, yhat);
 
-    return errors.map(error => error**2).reduce((a,b) => a+b, 0) / errors.length;
+    var avg = yhat.reduce((a, b) => a + b, 0) / yhat.length;
+    var varYHat = yhat.map(yhat => (yhat - avg) ** 2).reduce((a, b) => a + b, 0) / (errors.length - 1);
+    $('#train-variance').text(varYHat.toPrecision(3));
+
+    var trainBias = fx.map(f => (f-avg)**2).reduce((a,b) => a+b,0) / (fx.length-1);
+    var trainBiasSquared = trainBias**2;
+    $('#train-bias').text(trainBiasSquared.toPrecision(3));
+
+    return errors.map(error => error ** 2).reduce((a, b) => a + b, 0) / (errors.length - 1);
   }
 
   function yHat(xdata, coefficients) {
-    var A = getVandermonde(xdata, coefficients.length-1);
+    var A = getVandermonde(xdata, coefficients.length - 1);
     return numeric.dot(A, coefficients);
   }
 
@@ -365,13 +386,13 @@ $(function() {
     }
   }
 
-  $('.new-points').click(function() {
+  $('.new-points').click(function () {
     resetPoints();
     $('#train-err').text('N/A');
     $('#test-err').text('N/A');
   });
 
-  $('#increment-degree').click(function() {
+  $('#increment-degree').click(function () {
     $('#decrement-degree').removeAttr('disabled');
     modelDegree += 1;
     updateModelDegree();
@@ -380,7 +401,7 @@ $(function() {
     }
   });
 
-  $('#decrement-degree').click(function() {
+  $('#decrement-degree').click(function () {
     $('#increment-degree').removeAttr('disabled');
     modelDegree -= 1;
     updateModelDegree();
@@ -389,7 +410,7 @@ $(function() {
     }
   });
 
-  $('#increment-num-points').click(function() {
+  $('#increment-num-points').click(function () {
     $('#decrement-num-points').removeAttr('disabled');
     numPoints += 5;
     $('#num-points').text(numPoints);
@@ -399,7 +420,7 @@ $(function() {
     resetPoints();
   });
 
-  $('#decrement-num-points').click(function() {
+  $('#decrement-num-points').click(function () {
     $('#increment-num-points').removeAttr('disabled');
     numPoints -= 5;
     $('#num-points').text(numPoints);
@@ -434,6 +455,10 @@ $(function() {
   function random_normal(mean, variance) {
     var normalFn = d3.random.normal(mean, variance);
     return normalFn();
+  }
+
+  function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
   }
 
 });
